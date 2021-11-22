8a8a59dfd3ef943043a6700b3dfc457f2c937d2c
TuanKhanh <19521681@gm.uit.edu.vn>
2021-11-22 19:23:58 +0700

    cart p2

--- front-end/src/javascript/process.js
+++ front-end/src/javascript/process.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
        modal.style.display = "none";
      }
      order.onclick = function () {
-       alert("Cảm ơn bạn đã thanh toán đơn hàng")
+       window.location.href='pay.html';
      }
      window.onclick = function (event) {
        if (event.target == modal) {
